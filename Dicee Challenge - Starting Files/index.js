52c45e272259689465041679e77c61f1e14b6803
zzzbnbzzz <65437810+zzzbnbzzz@users.noreply.github.com>
2020-06-11 23:12:22 -0700

    Add files via upload

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,18 @@
+
+var randomNumber1 = Math.floor(Math.random()*6 + 1);
+var randomDice1 = "dice" + randomNumber1 + ".png";
+var randomDiceSource1 = "images/" + randomDice1;
+document.querySelectorAll("img")[0].setAttribute("src",randomDiceSource1);
+
+var randomNumber2 = Math.floor(Math.random()*6 + 1);
+var randomDice2 = "dice" + randomNumber2 + ".png";
+var randomDiceSource2 = "images/" + randomDice2;
+document.querySelectorAll("img")[1].setAttribute("src",randomDiceSource2);
+
+if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "🎉Player 2 Wins!"
+} else if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🎉Player 1 Wins!"
+} else {
+  document.querySelector("h1").innerHTML = "🤭Draw!"
+}
